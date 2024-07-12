e2fc1d1a6db9903cb84f89ff26d115580738aa52
theirish81 <theirish81@gmail.com>
2024-07-12 14:10:53 +0200

    removing garbage

--- main.go
+++ main.go
@@ -9,11 +9,4 @@ func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
 		log.Fatalln(err)
 	}
-	//data, _ := os.ReadFile("test_data/github.yaml")
-	//doc, err := oapi.NewDoc(data)
-	//fmt.Println(err)
-	//sx := server.NewServer(8080, doc)
-	//if err := sx.Run(); err != nil {
-	//	log.Panic(err)
-	//}
 }
