c420301fcdd622bc494d6c2caab7fc620b96dd1f
theirish81 <theirish81@gmail.com>
2024-07-16 10:05:45 +0200

    some tests

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,29 @@
+package evaluator
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestEvaluator_Load(t *testing.T) {
+	ev := NewEvaluator()
+	data, err := ev.Load("../test_data/sample_payload.json")
+	assert.Nil(t, err)
+	assert.Greater(t, len(data), 0)
+}
+
+func TestEvaluator_RunString(t *testing.T) {
+	ev := NewEvaluator()
+	ev.Set("foo", "bar")
+	v, err := ev.RunString("foo")
+	assert.Nil(t, err)
+	assert.Equal(t, "bar", v)
+}
+
+func TestEvaluator_Template(t *testing.T) {
+	ev := NewEvaluator()
+	ev.Set("foo", "bar")
+	v, err := ev.Template("{{foo}}")
+	assert.Nil(t, err)
+	assert.Equal(t, "bar", v)
+}
--- jsf/faker_test.go
+++ jsf/faker_test.go
@@ -2,6 +2,7 @@ package jsf
 
 import (
 	"github.com/stretchr/testify/assert"
+	"net/url"
 	"testing"
 )
 
@@ -10,4 +11,8 @@ func TestFake(t *testing.T) {
 	assert.IsType(t, float32(32.5), Fake("float"))
 	assert.IsType(t, "foobar", Fake("address"))
 	assert.IsType(t, "foobar", Fake("sblargh"))
+	assert.IsType(t, true, Fake("boolean"))
+
+	_, err := url.Parse(Fake("url").(string))
+	assert.Nil(t, err)
 }
